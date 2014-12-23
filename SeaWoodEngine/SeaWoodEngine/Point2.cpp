0da2cacea10f1912e6b26e0d44f4d8d5405a7f9b
jwvg0425 <jwvg0425@naver.com>
2014-12-23 16:27:59 +0900

    2차원 좌표 구조체 Point 추가

---
+++ SeaWoodEngine/SeaWoodEngine/Point2.cpp
@@ -0,0 +1,88 @@
+﻿#include "stdafx.h"
+#include "Point2.h"
+#include <cfloat>
+#include <math.h>
+#include <stdlib.h>
+
+
+USING_NS_SW;
+
+SeaWood::Point2::Point2(float x, float y)
+{
+	m_X = x;
+	m_Y = y;
+}
+
+SeaWood::Point2::Point2(const Point2& other)
+{
+	m_X = other.m_X;
+	m_Y = other.m_Y;
+}
+
+SeaWood::Point2::~Point2()
+{
+
+}
+
+Point2& SeaWood::Point2::operator=(const Point2& other)
+{
+	if (this != &other)
+	{
+		this->m_X = other.m_X;
+		this->m_Y = other.m_Y;
+	}
+
+	return *this;
+}
+
+bool SeaWood::Point2::operator==(const Point2& other)
+{
+	float xDiff = fabs(other.m_X - this->m_X);
+	float yDiff = fabs(other.m_Y - this->m_Y);
+	float xLargest = max(fabs(this->m_X), fabs(other.m_X));
+	float yLargest = max(fabs(this->m_Y), fabs(other.m_Y));
+	
+	
+	if (xDiff <= xLargest*FLT_EPSILON && yDiff <= yLargest*FLT_EPSILON)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool SeaWood::Point2::operator!=(const Point2& other)
+{
+	return !(*this == other);
+}
+
+const Point2 SeaWood::Point2::operator+(const Point2& other)
+{
+	Point2 point = *this;
+
+	point.m_X += other.m_X;
+	point.m_Y += other.m_Y;
+
+	return point;
+}
+
+const Point2 SeaWood::Point2::operator-(const Point2& other)
+{
+	Point2 point = *this;
+
+	point.m_X -= other.m_X;
+	point.m_Y -= other.m_Y;
+
+	return point;
+
+}
+
+float SeaWood::Point2::getDistance(const Point2& other)
+{
+	double xDiff = this->m_X - other.m_X;
+	double yDiff = this->m_Y - other.m_Y;
+
+	return static_cast<float>(sqrt(xDiff*xDiff + yDiff*yDiff));
+}
---
+++ SeaWoodEngine/SeaWoodEngine/Point2.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+
+NS_SW_BEGIN
+
+
+// 2차원 좌표계의 한 점을 나타내는 구조체.
+struct Point2
+{
+public:
+	Point2(float x, float y);
+	~Point2();
+	Point2(const Point2& other);
+	Point2& operator =(const Point2& other);
+
+	//실수 오차를 고려하여 두 좌표가 같은 좌표인지를 비교한다.
+	bool operator ==(const Point2& other);
+
+	//실수 오차를 고려하여 두 좌표가 같은 좌표인지를 비교한다.
+	bool operator !=(const Point2& other);
+
+	//두 좌표의 값을 단순 더한 좌표를 리턴한다.
+	const Point2 operator +(const Point2& other);
+
+	//두 좌표의 값을 단순하게 뺀 좌표를 리턴한다.
+	const Point2 operator -(const Point2& other);
+
+	//두 좌표 사이의 거리를 계산하여 리턴한다.
+	float getDistance(const Point2& other);
+
+public:
+	float m_X, m_Y;
+};
+
+NS_SW_END
