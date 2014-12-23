e4f9a98f341affb21218ed356d5e5bee239516eb
jwvg0425 <jwvg0425@naver.com>
2014-12-23 16:52:59 +0900

    rect 구조체 기본형 추가

---
+++ SeaWoodEngine/SeaWoodEngine/Rect.cpp
@@ -0,0 +1,50 @@
+﻿#include "stdafx.h"
+#include "Rect.h"
+
+USING_NS_SW;
+
+SeaWood::Rect::Rect() : m_Origin(Point2::ZERO), m_Size(Size::ZERO)
+{
+}
+
+SeaWood::Rect::Rect(float x, float y, float width, float height)
+: m_Origin(x, y), m_Size(width, height)
+{
+}
+
+SeaWood::Rect::Rect(const Point2& point, const Size& size)
+: m_Origin(point), m_Size(size)
+{
+
+}
+
+SeaWood::Rect::Rect(const Point2& point, float width, float height)
+: m_Origin(point), m_Size(width, height)
+{
+
+}
+
+SeaWood::Rect::Rect(float x, float y, const Size& size)
+: m_Origin(x, y), m_Size(size)
+{
+
+}
+
+SeaWood::Rect::Rect(const Rect& rect)
+{
+	this->m_Origin = rect.m_Origin;
+	this->m_Size = rect.m_Size;
+}
+
+SeaWood::Rect::~Rect()
+{
+
+}
+
+Rect& SeaWood::Rect::operator=(const Rect& other)
+{
+	this->m_Origin = other.m_Origin;
+	this->m_Size = other.m_Size;
+
+	return *this;
+}
---
+++ SeaWoodEngine/SeaWoodEngine/Rect.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include "Point2.h"
+#include "Size.h"
+
+
+NS_SW_BEGIN
+
+//사각형을 나타내는 구조체.
+struct Rect
+{
+public:
+	Rect();
+	~Rect();
+	Rect(float x, float y, float width, float height);
+	Rect(const Point2& point, const Size& size);
+	Rect(const Point2& point, float width, float height);
+	Rect(float x, float y, const Size& size);
+	Rect(const Rect& rect);
+
+	Rect& operator =(const Rect& other);
+
+public:
+	Point2 m_Origin;
+	Size m_Size;
+};
+
+NS_SW_END
